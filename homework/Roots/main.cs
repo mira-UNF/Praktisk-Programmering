7a667fff1d719151533927f27747dc57c4335a0f
Mikkel Rasmussen <mikkelrasmussen@Mikkels-MacBook-Pro-3.local>
2025-05-27 17:16:48 +0200

    finally finished the roots part B

--- homework/Roots/main.cs
+++ homework/Roots/main.cs
@@ -58,8 +58,137 @@ class main{
 
 		/*-----------------------------PART B------------------------------*/
 
-		
+		/*Initalizing parameters*/
+		double rmin = 0.1;
+		double rmax = 10.0;
+		double acc = 0.01;
+		double eps = 0.01;
 
+		/*Initializing generic lists to hold the last integration run for later comparison with anaylytical solution*/
+		genlist<double> xs = new genlist<double>();
+		genlist<vector> ys = new genlist<vector>();
+
+		/*Defining function that outputs the wavefunction value at the boundary as a function of guessed energy*/
+		/*Idea is that the function here solves for the wavefunction for a given E and outputs the value of the*/
+		/*wavefunction at rmax, then we use newton to find the roots of this function, aka what E yields a*/
+		/*wavefunction that goes to zero at rmax.*/
+
+		Func<vector, vector> wavefunction_solver = delegate(vector E_guess)
+		{
+			double E = E_guess[0]; //energy guess
+
+			Func<double, vector, vector> radial_SE = delegate(double r, vector y){ //radial SE to be solved
+				return new vector(y[1], -2*(1/r+E)*y[0]);
+			};
+
+			vector init = new vector(rmin-rmin*rmin, 1-2*rmin); //initial conditions as stated in the exercise
+
+			vector solution = ode.driver(radial_SE,rmin,init,rmax,xs,ys, acc: acc, eps: eps);
+
+			return new vector(solution[0]);
+		};
+
+
+		vector result = roots.newton(wavefunction_solver, new vector(-1.0));
+
+		xs = new genlist<double>();
+		ys = new genlist<vector>();
+
+		vector final_wavefunction = wavefunction_solver(new vector(result[0]));
+
+		var out_wave = new StreamWriter("wavefunction_n.txt");
+		for(int j = 0; j < xs.size; j++) {
+			out_wave.WriteLine($"{xs[j].ToString(CultureInfo.InvariantCulture)}	{ys[j][0].ToString(CultureInfo.InvariantCulture)}");
+		}
+		out_wave.Close();
+
+		WriteLine("");
+		WriteLine("----------------PART B------------------");
+		WriteLine($"The found root is: E0 = {result[0]}");
+		WriteLine("Whereas the expected result is -0.5.");
+
+		WriteLine("A plot of the numerical wavefunction (dots) vs the analytical one (line) can be found in wavefunction.png");
+
+		var out_wave2 = new StreamWriter("wavefunction_a.txt");
+		for(int j = 0; j < xs.size; j++){
+			out_wave2.WriteLine($"{xs[j].ToString(CultureInfo.InvariantCulture)} {(xs[j]*Exp(-xs[j])).ToString(CultureInfo.InvariantCulture)}");
+		}
+		out_wave2.Close();
+
+		var out_energy = new StreamWriter("true_groundstate.txt");
+		for(int j = 0; j < 6; j++){
+			out_energy.WriteLine($"{((double) j).ToString(CultureInfo.InvariantCulture)} {(-0.5).ToString(CultureInfo.InvariantCulture)}");
+		}
+		out_energy.Close();
+
+		/*Investigating convergences*/
+		WriteLine("");
+		WriteLine("The test of convergence can be found in the seperate .png files named after each parameter that is varied");
+		WriteLine("The analytical value is plotted as a line, while the numerical values are plotted as dots");
+
+		int n = 5; //maximum number of iterations for convergence tests
+		double E_guess2 = -0.8;
+
+		/*rmax test*/
+		var out_rmax = new StreamWriter("rmax.txt");
+		WriteLine("Doing rmax convergence test...");
+		for(int j = 1; j < n+1; j++ ){
+			WriteLine($"{j} out of {n}");
+			rmax = (double) j; //here the (double) explicitely typecasts j as a double
+			vector result_rmax = roots.newton(wavefunction_solver, new vector(E_guess2));
+			out_rmax.WriteLine($"{rmax.ToString(CultureInfo.InvariantCulture)} {result_rmax[0].ToString(CultureInfo.InvariantCulture)}");
+		}
+		out_rmax.Close();
+		WriteLine("");
+		WriteLine("rmax convergence test done");
+
+		rmax = 10.0;
+
+		/*rmin test*/
+		var out_rmin = new StreamWriter("rmin.txt");
+		WriteLine("");
+		WriteLine("Doing  rmin convergence test...");
+		for(int j = 1; j < n+1; j++ ){
+			WriteLine($"{j} out of {n}");
+			rmin = (double) j/10.0;
+			vector result_rmin = roots.newton(wavefunction_solver, new vector(-1.0));
+			out_rmin.WriteLine($"{rmin.ToString(CultureInfo.InvariantCulture)} {result_rmin[0].ToString(CultureInfo.InvariantCulture)}");
+		}
+		out_rmin.Close();
+		WriteLine("");
+		WriteLine("rmin convergence test done");
+
+		rmin = 0.1;
+
+		/*acc test*/
+		WriteLine("");
+		WriteLine("Doing acc convergence test...");
+		var out_acc = new StreamWriter("acc.txt");
+		for(int j = 1; j < n+1; j++ ){
+			WriteLine($"{j} out of {n}");
+			acc = (double) j/100;
+			vector result_acc = roots.newton(wavefunction_solver, new vector(-1.0));
+			out_acc.WriteLine($"{acc.ToString(CultureInfo.InvariantCulture)} {result_acc[0].ToString(CultureInfo.InvariantCulture)}");
+		}
+		out_acc.Close();
+		WriteLine("");
+		WriteLine("acc convergence test done");
+
+		acc = 0.01;
+
+		/*eps test*/
+		WriteLine("");
+		WriteLine("Doing eps convergence test...");
+		var out_eps = new StreamWriter("eps.txt");
+		for(int j = 1; j < n+1; j++ ){
+			WriteLine($"{j} out of {n}");
+			eps = (double) j/100;
+			vector result_eps = roots.newton(wavefunction_solver, new vector(-1.0)); 
+			out_eps.WriteLine($"{eps.ToString(CultureInfo.InvariantCulture)} {result_eps[0].ToString(CultureInfo.InvariantCulture)}");
+		}
+		out_eps.Close();
+		WriteLine("");
+		WriteLine("eps convergence test done");
 
 	} //End of Main()
 
