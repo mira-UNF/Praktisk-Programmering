a46e3f8d8a762577c7d4b6b0155d212ff667a4e7
Mikkel Rasmussen <mikkelrasmussen@Mikkels-MacBook-Pro-3.local>
2025-06-16 15:26:25 +0200

    completed also part B in ANN

--- homework/ANN/main.cs
+++ homework/ANN/main.cs
@@ -48,12 +48,73 @@ class main{
 		WriteLine("");
 		WriteLine("---------------------------PART B-------------------------------");
 		WriteLine("The neural network can also output derivative, double derivative and anti derivative responses");
+		WriteLine("The different derivatives can be found in the graphs named plot_deriv.png, plot_dderiv.png etc.");
+		WriteLine("The normal function can be found in plot_B.png");
+		WriteLine("Again the circles are the data points/tabulated values and the solid line is the network response");
 
 		/*Preparing training data*/
+		Func<double, double> fB = z => Cos(z); //Function to approximate
+
+		int lengthB = 30; //number of datapoints
+		double aB = -PI; //start of interval
+		double bB = PI; //end of interval
+
+		double[] xB = new double[lengthB]; //x data
+		double[] yB = new double[lengthB]; //y data
+
+		/*Data to compare the different derivatives and anti derivates to*/
+		Func<double, double> dfB = z => -Math.Sin(z);      // First derivative
+		Func<double, double> ddfB = z => -Math.Cos(z);     // Second derivative
+		Func<double, double> adfB = z => Math.Sin(z);      // Antiderivative
+
+		var deriv_data = new StreamWriter("deriv_data.txt");
+		var dderiv_data = new StreamWriter("dderiv_data.txt");
+		var aderiv_data = new StreamWriter("aderiv_data.txt");
+		var training_dataB = new StreamWriter("training_dataB.txt");
+
+		for(int i = 0; i<lengthB; i++){
+			xB[i] = aB + (bB-aB)*i/(lengthB-1); //creating even spread over interval
+			yB[i] = fB(xB[i]);
+			double yd = dfB(xB[i]);
+			double ydd = ddfB(xB[i]);
+			double yad = adfB(xB[i]);
+
+			training_dataB.WriteLine($"{xB[i].ToString(CultureInfo.InvariantCulture)} {yB[i].ToString(CultureInfo.InvariantCulture)}");
+			deriv_data.WriteLine($"{xB[i].ToString(CultureInfo.InvariantCulture)} {yd.ToString(CultureInfo.InvariantCulture)}");
+			dderiv_data.WriteLine($"{xB[i].ToString(CultureInfo.InvariantCulture)} {ydd.ToString(CultureInfo.InvariantCulture)}");
+			aderiv_data.WriteLine($"{xB[i].ToString(CultureInfo.InvariantCulture)} {yad.ToString(CultureInfo.InvariantCulture)}");
+
+
+		}
+		training_dataB.Close();
+		deriv_data.Close();
+		dderiv_data.Close();
+		aderiv_data.Close();
+
 
 		/*Setting up and training neural network*/
+		ann ann_NewtonB = new ann(3); //creating the neural network
+
+                ann_NewtonB.train(xB,yB,1e-6,5000); //training the network on the data
+
 
 		/*Outputting network responses*/
+		var network_responseB = new StreamWriter("network_responseB.txt");
+		var deriv_response = new StreamWriter("deriv_response.txt");
+		var dderiv_response = new StreamWriter("dderiv_response.txt");
+		var aderiv_response = new StreamWriter("aderiv_response.txt");
+
+		for(int i = 0; i<length; i++){
+			network_responseB.WriteLine($"{xB[i].ToString(CultureInfo.InvariantCulture)} {ann_NewtonB.response(xB[i]).ToString(CultureInfo.InvariantCulture)}");
+			deriv_response.WriteLine($"{xB[i].ToString(CultureInfo.InvariantCulture)} {ann_NewtonB.derivativeResponse(xB[i]).ToString(CultureInfo.InvariantCulture)}");
+			dderiv_response.WriteLine($"{xB[i].ToString(CultureInfo.InvariantCulture)} {ann_NewtonB.dderivativeResponse(xB[i]).ToString(CultureInfo.InvariantCulture)}");
+			aderiv_response.WriteLine($"{xB[i].ToString(CultureInfo.InvariantCulture)} {(ann_NewtonB.antiderivativeResponse(xB[i])-ann_NewtonB.antiderivativeResponse(0)).ToString(CultureInfo.InvariantCulture)}");
+
+		}
+		network_responseB.Close();
+		deriv_response.Close();
+		dderiv_response.Close();
+		aderiv_response.Close();
 
 
 	} //end of Main()
